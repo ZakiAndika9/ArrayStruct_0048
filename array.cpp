e8585f62ef9ae59eb219024d1b677200d34f73c8
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-04-14 11:31:08 +0700

    2

--- array.cpp
+++ array.cpp
@@ -9,3 +9,29 @@ string nama[5]={"ben" , "agus" , "fara" , "ama" , "akila"};
 
 //mendeklarasikan array dengan ukuran dan tanpa nilai awal
 float score[5];
+
+int main(){
+	cout << "Nama pada index 4 adalah " << nama[4] << endl;
+	//memberikan nilai pada elemen array per index
+	nama[4]= "zaki";
+	cout << "Nama pada index 4 adalah " << nama[4] << endl;
+
+	cout << "inputscore" << endl;
+	cout << endl;
+
+	//memberi nilai array dengan looping for
+	for(int i = 0; i<5; i++){
+		cout << "score ke-" << i+1 << "=";
+		cin >> score[i];
+
+	}
+
+	cout << "Tampilkan Score " << endl;
+	cout << endl;
+
+	//memberi nilai array dengan looping for
+	for (int i = 0; i<5; i++) {
+		cout << "Score ke-" << i+1 << "=";
+		cin >> score[i];
+	}
+}
